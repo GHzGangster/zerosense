67eb2740ad4b059f55697b430684bc7969af4588
GHzGangster <infalen@gmail.com>
2018-09-28 15:16:44 +0000

    x

--- ZsArray.js
+++ ZsArray.js
@@ -76,7 +76,7 @@ class ZsArray {
 			for (var i = 0; i < 0x300; i++) {
 				str = this.memoryReader.read(addr3 + i, this.array[1].length * 2);
 				if (str === this.array[1]) {
-					zs.logger.debug(`Search 1: Found string 0x${(addr3 + i).toString(16)}  +0x{i.toString(16)}  0x${(this.array[1].length * 2).toString(16)}`);
+					zs.logger.debug(`Search 1: Found string 0x${(addr3 + i).toString(16)}  +0x${i.toString(16)}  0x${(this.array[1].length * 2).toString(16)}`);
 					addrStr = addr3 + i;
 					break;
 				}
