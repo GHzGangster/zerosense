d4ac6b51430a9004e6d75aefb7c40cc6faf663d1
GHzGangster <infalen@gmail.com>
2019-03-30 00:17:22 +0000

    Add 4.84 DEX offsets

--- Offsets.js
+++ Offsets.js
@@ -30,6 +30,38 @@ const DEX_481 = {
 	
 };
 
+const DEX_484 = {
+	
+	toc: 0x705648,
+	tocEntry4: 0x750000,
+	
+	////////////
+	
+	gadget1: 0x976BC,
+	gadget6: 0x61D8F8,
+	
+	gadgetMod1: 0x616BF4,
+	gadgetMod2: 0x13B74,
+	gadgetMod3: 0xB8EB8,
+	gadgetMod4a: 0xDEBD8,
+	gadgetMod5: 0x42B4D0,
+	gadgetMod8: 0x2C24E0,
+	gadgetMod13: 0x33E484,
+	gadgetMod15: 0x3A4C2C,
+	
+	////////////
+	
+	tocZ1: 0x7580C0, // _sysPrxForUser_
+	
+	gadgetZ1: 0x30457C,
+	gadgetZ2: 0x61D8F4, // _sysPrxForUser__sys_malloc
+	gadgetZ3: 0x61D88C, // _sysPrxForUser__sys_free
+	
+	gadgetZMod1: 0xD62B4,
+	gadgetZMod2: 0x105F0,
+	
+};
+
 
 function get(env) {
 	var dex = env.dex;
