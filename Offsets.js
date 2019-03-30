3fa1eea8144142a8663ba0d7d5c8fabef2cefd26
GHzGangster <infalen@gmail.com>
2019-03-30 00:04:40 +0000

    Add 4.84 DEX offsets.

--- Offsets.js
+++ Offsets.js
@@ -11,10 +11,27 @@ var DEX_481 = {
 	gadgetMod15: 0x3A4C28,
 	
 	gadgetZ1: 0x304578,
-	
 	gadgetZMod1: 0xD62B4,
 	gadgetZMod2: 0x105F0,
 };
+
+var DEX_484 = {
+	addrToc: 0x705648,
+	gadget1: 0x976BC,
+	gadgetMod1: 0x616BF4,
+	gadgetMod2: 0x13B74,
+	gadgetMod3: 0xB8EB8,
+	gadgetMod4a: 0xDEBD8,
+	gadgetMod5: 0x42B4D0,
+	gadgetMod8: 0x2C24E0,
+	gadgetMod13: 0x33E484,
+	gadgetMod15: 0x3A4C2C,
+	
+	gadgetZ1: 0x30457C,
+	gadgetZMod1: 0xD62B4,
+	gadgetZMod2: 0x105F0,
+};
+
 
 
 function get(env) {
@@ -23,6 +40,8 @@ function get(env) {
 	
 	if (fw === "4.81" && dex) {
 		return DEX_481;
+	} else if (fw === "4.84" && dex) {
+		return DEX_484;
 	}
 	
 	throw new Error("No offsets for environment.");
