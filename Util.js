dd0655843d27b96421d224ffadd814936231f8ef
GHzGangster <infalen@gmail.com>
2019-04-05 03:16:29 +0000

    aaaa

--- Util.js
+++ Util.js
@@ -32,11 +32,11 @@ function int64(high, low) {
 }
 
 function getint16(val, offset = 0) {
-	return parseInt(hex16(val.charCodeAt(offset / 2)), 16);
+	return parseInt(hex16(val.charCodeAt(offset / 2)), 16) & 0xffff;
 }
 
 function getint32(val, offset = 0) {
-	return parseInt(hex16(val.charCodeAt(offset / 2)) + hex16(val.charCodeAt(offset / 2 + 1)), 16);
+	return parseInt(hex16(val.charCodeAt(offset / 2)) + hex16(val.charCodeAt(offset / 2 + 1)), 16) & 0xffffffff;
 }
 
 function setint32(str, offset, val) {
